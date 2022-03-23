1d5633c4e3896d6a732f9a28d6bff0e016e8d59d
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-23 08:12:31 -0700

    create file streaming with gRPC Service

---
+++ FileStreamClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using FileStreamServer;
+using Google.Protobuf;
+using Grpc.Core;
+using Grpc.Net.Client;
+using static System.Console;
+
+WriteLine("File Streaming With gRPC!");
+const int size = 1024;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7266");
+var client = new StreamFile.StreamFileClient(channel);
+
+Write("Enter Your File Address : ");
+var fileAddress = ReadLine();
+Metadata headers = new();
+var extension = Path.GetExtension(fileAddress);
+headers.Add("extension", extension ?? ".jpg");
+
+using Stream source = File.OpenRead(fileAddress ?? "");
+using var call = client.UpStreamFile(headers);
+
+byte[] buffer = new byte[size];
+var itemLength = source.Length / 100;
+var uploaded = 0;
+WriteLine(source.Length);
+int bytesRead = 0;
+while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+{
+    await call.RequestStream.WriteAsync(new() { Buffer = ByteString.CopyFrom(buffer) });
+    uploaded += bytesRead;
+    var itemUploaded = uploaded / itemLength;
+    WriteLine($"{itemUploaded}%");
+}
+
+await call.RequestStream.CompleteAsync();
+var result = await call;
+WriteLine(result.Status);
+
---
+++ FileStreamServer/FileStreamServer/Services/FileStreamService.cs
@@ -0,0 +1,40 @@
+﻿using FileStreamServer;
+using Grpc.Core;
+
+namespace FileStreamServer.Services;
+
+public class FileStreamService : StreamFile.StreamFileBase
+{
+    readonly IWebHostEnvironment _env;
+
+    public FileStreamService(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public override async Task<FileResult> UpStreamFile(IAsyncStreamReader<Chunk> requestStream, ServerCallContext context)
+    {
+        string folderPath = _env.ContentRootPath + "/Files/";
+        if (!Directory.Exists(folderPath))
+            Directory.CreateDirectory(folderPath);
+        string filePath = folderPath + Guid.NewGuid().ToString() + context.RequestHeaders.Get("extension").Value;
+        Stream fs = new FileStream(filePath, FileMode.Create);
+
+        try
+        {
+            await foreach (var item in requestStream.ReadAllAsync())
+            {
+                await fs.WriteAsync(item.Buffer.ToByteArray(), 0, item.Buffer.Length);
+            }
+            return new FileResult { Status = 200 };
+        }
+        catch
+        {
+            return new FileResult { Status = 500 };
+        }
+        finally
+        {
+            fs.Close();
+        }
+    }
+}
